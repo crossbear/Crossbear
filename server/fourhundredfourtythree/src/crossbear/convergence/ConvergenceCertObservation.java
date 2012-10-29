0ddf28399f7d6480c6fc5639dc203b33e720c703
Ralph Holz <pki@net.in.tum.de>
2012-10-29 17:38:31 +0100

    umlauts

--- server/fourhundredfourtythree/src/crossbear/convergence/ConvergenceCertObservation.java
+++ server/fourhundredfourtythree/src/crossbear/convergence/ConvergenceCertObservation.java
@@ -14,7 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with Crossbear.  If not, see <http://www.gnu.org/licenses/>.
 
-    Original authors: Thomas Riedmaier, Ralph Holz (TU München, Germany)
+    Original authors: Thomas Riedmaier, Ralph Holz (TU Muenchen, Germany)
 */
 
 package crossbear.convergence;
