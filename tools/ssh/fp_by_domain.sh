9a8eb90b290eca9620a1ae92de9bcd9692bf4f61
Ralph Holz <pki@net.in.tum.de>
2012-12-28 12:01:46 +0100

    much easier way to parse the IP

--- tools/ssh/fp_by_domain.sh
+++ tools/ssh/fp_by_domain.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for ip in $(nslookup $1 | grep Address | grep -v "Address:.*53" | sed -s 's/Address:\ //g'); do
+for ip in $(dig +short $1); do
   dig -t TXT $ip.cbssh.net.in.tum.de
 done
