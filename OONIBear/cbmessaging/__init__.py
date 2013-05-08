a75b41a808b7d7c59b7606cefd6dd135bbd0c749
Vedat Levi Alev <alev@in.tum.de>
2013-05-08 11:20:25 +0200

    fix module info

--- OONIBear/cbmessaging/__init__.py
+++ OONIBear/cbmessaging/__init__.py
@@ -2,4 +2,5 @@
 These are all message types in internal naming.
 """
 
-__all__ = ["Message", "CurServTime", "HTRepKnownCert", "HTRepNewCert", "PipNot", "PipReq"]
+__all__ = ["Message", "CurServTime", "HTRepKnownCert", "HTRepNewCert", "PipNot", "PipReq",
+           "MessageList", "MessageTypes"]
