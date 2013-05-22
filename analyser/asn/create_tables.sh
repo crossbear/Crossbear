41e4f279aa45643b2e633231c4f9e4768be74b67
Jan Seeger <jan.seeger@thenybble.de>
2013-05-22 13:38:20 +0200

    Rework of geoip and asn scanners. New table layout, no central script for running both scanners exists yet.

--- analyser/asn/create_tables.sh
+++ analyser/asn/create_tables.sh
@@ -2,49 +2,18 @@
 
 
 
-if [[ $# -ne 3 ]]; then
-	echo "$0 <schema name> <user name> <database name>"
+if [[ $# -ne 2 ]]; then
+	echo "$0 <user name> <database name>"
 	exit 1;
 fi
 
-sed "s/__SCHEMAPLACEHOLDER__/$1/g" << 'EOF' | psql -U $2 $3 -f- 
-DROP SCHEMA IF EXISTS __SCHEMAPLACEHOLDER__ CASCADE;
-CREATE SCHEMA __SCHEMAPLACEHOLDER__;
-
-SET search_path = __SCHEMAPLACEHOLDER__;
-
-
-CREATE LANGUAGE plpgsql;
-
-CREATE FUNCTION insert_unique_domain(new_fqdn VARCHAR) RETURNS INTEGER AS
-$$
-DECLARE
-	new_index INTEGER;
-BEGIN
-    SELECT domains.id FROM __SCHEMAPLACEHOLDER__.domains WHERE fqdn = new_fqdn LIMIT 1 INTO new_index;
-    IF new_index IS NOT NULL THEN
-	RETURN new_index;
-    ELSE
-	INSERT INTO __SCHEMAPLACEHOLDER__.domains (fqdn) VALUES (new_fqdn) RETURNING id INTO new_index;
-	RETURN new_index;
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
-
-
--- MAIN TABLE
-CREATE TABLE __SCHEMAPLACEHOLDER__.domains (
+psql -U $2 $3 -f- <<'EOF'
+CREATE TABLE asn_results (
     id SERIAL PRIMARY KEY,
-    fqdn VARCHAR(255) UNIQUE NOT NULL
-);
-
--- ASN IP TABLE
-CREATE TABLE __SCHEMAPLACEHOLDER__.asn (
-    id SERIAL PRIMARY KEY,
-    host_id INTEGER REFERENCES __SCHEMAPLACEHOLDER__.domains(id),
     ip VARCHAR(64) NOT NULL,
     -- it can happen that there is no ASN in the iptoasn_dat file for that ip
     -- therefore asn has to be allowed to be NULL
-    asn INTEGER NULL
+    asn INTEGER NULL,
+    scan_date timestamptz(2) DEFAULT now()
 );
 EOF
--- analyser/geoip/GeoIp_Scanner.py
+++ analyser/geoip/GeoIp_Scanner.py
@@ -17,151 +17,28 @@
 
 #from IpDbParser import IpDbParser
 import MaxMind as MaxMind
-import socket, sys
-import threading
+from misc.Scanner import Scanner
 
-import logging
-import sys
-import PyASN, socket
-import Queue
+class GeoIPScanner(Scanner):
+	def __init__(self, main_config_file_loc):
+		super(GeoIPScanner, self).__init__(main_config_file_loc)
+		
+		# Fetch information regarding MaxMind DB from glaobl config file
+		self.dbpath = self.mainConfig.get("geoloc", "dbpath")
+		self.maxMind = MaxMind.MaxMind(self.dbpath)
+
+	def start_scan(self, ips):
+		cur = self.db.cursor()
+		sql = "INSERT INTO geo_results (ip, city, region_name, region, time_zone, longitude, latitude, metro_code, country_code, country_code3, country_name, postal_code, dma_code, ipStart, ipEnd) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+		for ip in ips:
+			record = self.maxMind.queryDB(ip)
+			cur.execute(sql, tuple([record[i] for i in ['address', 'city', 'region_name', 'region', 'time_zone', 'longitude', 'latitude', 'metro_code', 'country_code', 'country_code3', 'country_name', 'postal_code', 'dma_code', 'ipStart', 'ipEnd']]))
+		cur.close()
 
 
-class GeoIPStats(object):
-	def __init__(self, dbQueue, prefix, domain):
-		self.dbQueue = dbQueue
-		self.prefix = prefix
-		
-		self.domain = domain
-		
-		self.ips_in_diff_cities = []
-		self.ips_in_diff_countries = []
-		self.nr_diff_ip_ranges = []
-		self.all_ips_same_country = 0 # handled as bool
-		
-	def calcStats(self, record):
-		"""
-			Calculates statistic values
-			for each IP address to a given
-			hostname
-		"""
-		# record['city'] is often None,
-		#	 if so, don't add
-		if record['city']: 
-			if record['city'] not in self.ips_in_diff_cities:
-				self.ips_in_diff_cities.append(record['city'])
-			
-		if record['country_code3'] not in self.ips_in_diff_countries:
-			self.ips_in_diff_countries.append(record['country_code3'])
-		
-		ipRange = (record['ipStart'], record['ipEnd'])
-		if ipRange not in  self.nr_diff_ip_ranges:
-			self.nr_diff_ip_ranges.append(ipRange)
-		
-	def storeEval(self):
-		"""
-			Stores the gathered stats
-		"""
-		sql = "INSERT INTO %seval_geo " % self.prefix
-		sql = sql + """(host_id, ips_in_diff_cities, 
-				ips_in_diff_countries, nr_diff_ip_ranges, 
-				all_ips_same_country) 
-				VALUES ("""+self.prefix+"""insert_unique_domain(%s),
-				%s, %s, %s, %s) """
-				
-		if len(self.ips_in_diff_countries) == 1:
-			self.all_ips_same_country = 1
-			
-		sql_data = (self.domain, len(self.ips_in_diff_cities), 
-			len(self.ips_in_diff_countries), len(self.nr_diff_ip_ranges), 
-			self.all_ips_same_country)
-		
-		self.dbQueue.put( (sql, sql_data) )
-		
-class GeoIPStore(object):
-	def __init__(self, dbQueue, prefix):
-		"""
-		@param dbQueue: Queue.Queue for passing things to store to StorageThread
-		@param prefix: prefix of schema
-		"""
-		self.dbQueue = dbQueue
-		self.prefix = prefix
-		
-	def storeDomainInfo(self, domain):
-		sql = "INSERT INTO %sdomains " % self.prefix
-		sql += "(fqdn, scan_time) VALUES (%s, (SELECT TIMESTAMP 'now'))"
-		
-		self.dbQueue.put( (sql, (domain,)) )
-		
-	def storeGeoInfo(self, domain, record):
-		"""
-		@param domain: domain name from url list
-		@param record: dictionary with all available information to given ip address
-		"""
-		sql = "INSERT INTO %sgeoInfo " % self.prefix
-		sql = sql + """(host_id, ip, city, region_name, region, time_zone,
-				longitude, latitude, metro_code, country_code, country_code3,
-				country_name, postal_code, dma_code, ipStart, ipEnd) 
-				VALUES ("""+self.prefix+"""insert_unique_domain(%s),
-				%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """
-				
-		sql_data = ( domain, record['address'], record['city'], 
-			record['region_name'], record['region'], record['time_zone'], 
-			record['longitude'], record['latitude'], record['metro_code'], 
-			record['country_code'], record['country_code3'], record['country_name'], 
-			record['postal_code'], record['dma_code'], 
-			record['ipStart'], record['ipEnd'] )
-		
-		self.dbQueue.put( (sql, sql_data) )
-	
-class GeoIPQueryThread(threading.Thread):
-	def __init__(self, dbQueue, taskQueue, prefix, cityDbPath, cityDbDatPath, cityDbDatPath_backup, maxmind_url):
-		"""
-			@param dbQueue: Queue.Queue for passing things to store to StorageThread
-			@param prefix: prefix of schema
-			@param maxMind: MaxMind object with connected city DB
-			...
-		"""
-		self.dbQueue = dbQueue
-		self.taskQueue = taskQueue
-		self.prefix = prefix
-		self.cityDbPath = cityDbPath
-		self.cityDbDatPath = cityDbDatPath
-		self.cityDbDatPath_backup = cityDbDatPath_backup
-		self.maxmind_url = maxmind_url
-		
-		self.maxMind = MaxMind.MaxMind( cityDbDatPath )
-		
-		threading.Thread.__init__(self)
-			
-	def run(self):
-		# make sure you got the most recent City DB file
-		maxUpdater = MaxMind.MaxMindUpdater(self.prefix, self.cityDbPath, 
-			self.cityDbDatPath, self.cityDbDatPath_backup , self.maxmind_url)
-		maxUpdater.update()
-		
-		# New domain, new geo ip stats
-		geoIpStore = GeoIPStore(self.dbQueue, self.prefix)
-		
-		while True:
-			# Get domain and IPs
-			domain, ip_list_for_domain = self.taskQueue.get()
-			try:
-				# Stats object
-				evalGeoIp = GeoIPStats(self.dbQueue, self.prefix, domain)
-				
-				# Store fqdn
-				geoIpStore.storeDomainInfo( domain )
-				
-				# Perform queries to MaxMind DB
-				for ip in ip_list_for_domain:
-					record = self.maxMind.queryDB(ip)
-					if not record:
-						continue
-					geoIpStore.storeGeoInfo( domain, record )
-					evalGeoIp.calcStats( record )
-				
-				# Finalize analysis on host, write to DB
-				evalGeoIp.storeEval()
-				del(evalGeoIp)
-			finally:
-				self.taskQueue.task_done()
+if __name__ == "__main__":
+	import misc.DomainHandler
+	ips = misc.DomainHandler.IPSupplier("geoip.config")
+	asns = GeoIPScanner("geoip.config")
+	asns.start_scan(ips.get(1))
+
