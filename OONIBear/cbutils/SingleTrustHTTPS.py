9d48399844eebd15b32a34070b821cdbee35b5b3
Vedat Levi Alev <alev@in.tum.de>
2013-06-30 00:35:27 +0200

    public key pinning first version implemented

--- OONIBear/cbutils/SingleTrustHTTPS.py
+++ OONIBear/cbutils/SingleTrustHTTPS.py
@@ -1,4 +1,6 @@
-from   httplib import HTTPSConnection
+from httplib import HTTPSConnection
+from X509toPubKey import extractPubKey
+from Crypto.Hash import SHA256  
 import ssl
 
 class SingleTrustHTTPS(HTTPSConnection):
@@ -11,7 +13,16 @@ class SingleTrustHTTPS(HTTPSConnection):
     *args -- arguments to HTTPSConnection(*args) -- see there
     """
     def __init__(self, cert, *args):
-        self.cert = cert
+        try:
+            # read the certificate
+            c   = open(cert, "r").read()
+            # extract the publickey information
+            pki = extractPubKey(c)
+            # store its SHA256 hash
+            self.pHash = SHA256.new(pki).digest()
+        except:
+            raise IOError, ("Can't open certificate: %s" % cert)
+        
         HTTPSConnection.__init__(self, *args)
     
 
@@ -20,19 +31,18 @@ class SingleTrustHTTPS(HTTPSConnection):
         Connect to the HTTPs server.
         """
         HTTPSConnection.connect(self)
-        # get server's certificate in DER format
+        # get the server certificate
         derc  = self.sock.getpeercert(True)
-        # convert trusted certificate to DER
-        servd = ssl.PEM_cert_to_DER_cert(open(self.cert, "r").read())
-
-
-        # TODO: future versions should pin to the public key,
-        # not to the entire certificate. Will make it easier
-        # to replace/renew the certificate.
-        # Ticket is opened and assigned to Ralph.
+        # the following conversion is a bit backwards
+        # but seems to do the trick
+        pemc  = ssl.DER_cert_to_PEM_cert(derc)
+        # extract the public key info
+        spki  = extractPubKey(pemc)
+        # compute the sha256 hash
+        sHash = SHA256.new(spki).digest()
 
         # compare the certificates
-        if derc != servd:
+        if self.pHash != sHash:
             raise ValueError, "Certificate of the server could not be validated!"
         
 
