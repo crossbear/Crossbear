fa67c1769f3e48ea8d819f44408263e609507c75
Jan Seeger <jan.seeger@thenybble.de>
2013-08-27 17:08:31 +0200

    - Added options to create a graph containing only the bad traces and all good traces intersecting with them.

--- analyser/graph-traceroute/graph-traceroute.py
+++ analyser/graph-traceroute/graph-traceroute.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python
 from database import DB
 from graphfactory import GraphFactory
+import argparse
 
+parser = argparse.ArgumentParser(description="Graphs crossbear traces.")
+parser.add_argument("-i", "--intersecting", action = "store_true")
+
+args = parser.parse_args()
 db = DB("analyser.config")
-gf = GraphFactory()
+graphoptions = {}
+if (args.intersecting):
+    graphoptions = {"intersecting": True}
+gf = GraphFactory(**graphoptions)
 g = gf.tograph(db.traces(1))
 g.draw_to_json("out.json")
--- analyser/graph-traceroute/graphfactory.py
+++ analyser/graph-traceroute/graphfactory.py
@@ -16,26 +16,70 @@ class GraphFactory(object):
         return self.sources_for_certificate(cert, certlist, "CrossbearCVR") == []
 
     def tograph(self, result):
+        if "intersecting" in self.options:
+            return self.to_intersecting_graph(result)
+        else:
+            return self.to_regular_graph(result)
+
+    def intersects_bad_trace(self, htr, badhtrs):
+        totest = htr.trace()
+        badtraces = [h.trace() for h in badhtrs]
+        
+        for trace_elem in totest.trace_elems():
+            for badtrace in badtraces:
+                if trace_elem in badtrace.trace_elems():
+                    return True
+        return False
+
+    # Draw bad certificates first
+    def to_intersecting_graph(self,result):
+        # first, collect bad traces.
         g = graph.Graph()
         htrs = result.results()
+        badhtrs = []
+        todraw = []
+        for htr in htrs:
+            if not self.comes_from_server(htr.certificate(), result.certificates()):
+                badhtrs.append(htr)
+        todraw.extend(badhtrs)
+        # find all intersecting traces
+        for htr in htrs:
+            if not htr in badhtrs:
+                if self.intersects_bad_trace(htr, badhtrs):
+                    todraw.append(htr)
+        
+        return self.htrs_to_graph(todraw, result.certificates())
+
+        
+    def htrs_to_graph(self, htrs, certificates):
+        g = graph.Graph()
         for htr in htrs:
             t = htr.trace()
             for te in t.trace_elems():
                 for i in te.ips():
                     g.add_node(i)
-                    g.add_node_attribute(i, "geo", te.geo(i))
-                    g.add_node_attribute(i, "asn", te.asn(i))
-            for i in t.trace_elems()[0].ips():
-                g.add_node_attribute(i, "start", "true")
-                g.add_node_attribute(i, "fromserver", self.comes_from_server(htr.certificate(), result.certificates()))
-                g.add_node_attribute(i, "fromcvr", self.comes_from_cvr(htr.certificate(), result.certificates()))
-                g.add_node_attribute(i, "certificate", htr.certificate().hash())
-            for i in t.trace_elems()[-1].ips():
-                g.add_node_attribute(i, "end", "true")
+                    g.set_node_attribute(i, "geo", te.geo(i))
+                    g.set_node_attribute(i, "asn", te.asn(i))
+
+            g.set_node_attribute(t.source(), "start", "true")
+            g.set_node_attribute(t.source(), "fromserver",self.comes_from_server(
+                htr.certificate(), certificates))
+            g.set_node_attribute(t.source(), "fromcvr",self.comes_from_cvr(
+                htr.certificate(), certificates))
+            g.set_node_attribute(t.source(), "certificate", htr.certificate().hash())
+            g.set_node_attribute(t.target(), "end", "true")
+            
             for i in range(1, len(t.trace_elems())):
                 lastelements = t.trace_elem(i - 1)
                 thiselements = t.trace_elem(i)
                 for ip1 in lastelements.ips():
                     for ip2 in thiselements.ips():
                         g.add_edge(ip1, ip2)
+                        g.add_edge_attribute(ip1, ip2, "tracesource", t.source())
         return g
+            
+                
+
+    def to_regular_graph(self, result):
+        return self.htrs_to_graph(result.results(), result.certificates());
+
